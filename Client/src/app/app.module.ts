93280e850d24fdccfecb719d5d8a7a826a4cdd06
MohamedAmine-Oueslati <wesamin1991@gmail.com>
2020-08-20 19:18:13 +0100

    signup+login

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,35 +1,49 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ToastrModule } from 'ngx-toastr';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AngularMaterialModule } from "./homecomponents/homecomponents.module";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { CommonModule } from "@angular/common";
 
-import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { ToastrModule } from "ngx-toastr";
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app.routing";
+import { ComponentsModule } from "./components/components.module";
 
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AppComponent } from "./app.component";
+
+import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
+import { LogInComponent } from "./homecomponents/log-in/log-in.component";
+import { RegisterComponent } from "./homecomponents/register/register.component";
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
+    BrowserModule,
+    AngularMaterialModule,
     FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
     HttpClientModule,
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
     NgbModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    CommonModule,
   ],
   declarations: [
     AppComponent,
-    AdminLayoutComponent
-
+    AdminLayoutComponent,
+    LogInComponent,
+    RegisterComponent,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
--- Client/src/app/layouts/admin-layout/admin-layout.module.ts
+++ Client/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AdminLayoutRoutes } from './admin-layout.routing';
-import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { ChartsModule } from 'ng2-charts/ng2-charts';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ToastrModule } from 'ngx-toastr';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AdminLayoutRoutes } from "./admin-layout.routing";
+import { DashboardComponent } from "../../dashboard/dashboard.component";
+import { UserProfileComponent } from "../../user-profile/user-profile.component";
+import { TableListComponent } from "../../table-list/table-list.component";
+import { TypographyComponent } from "../../typography/typography.component";
+import { IconsComponent } from "../../icons/icons.component";
+// import { MapsComponent } from '../../maps/maps.component';
+import { NotificationsComponent } from "../../notifications/notifications.component";
+import { ChartsModule } from "ng2-charts/ng2-charts";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { ToastrModule } from "ngx-toastr";
+// import { UpgradeComponent } from '../../upgrade/upgrade.component';
 
 @NgModule({
   imports: [
@@ -22,18 +22,17 @@ import { UpgradeComponent } from '../../upgrade/upgrade.component';
     FormsModule,
     ChartsModule,
     NgbModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
   ],
   declarations: [
     DashboardComponent,
     UserProfileComponent,
     TableListComponent,
-    UpgradeComponent,
+    // UpgradeComponent,
     TypographyComponent,
     IconsComponent,
-    MapsComponent,
+    // MapsComponent,
     NotificationsComponent,
-  ]
+  ],
 })
-
 export class AdminLayoutModule {}
